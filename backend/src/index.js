5b43c4b51b88e30b00863230d220a015af6d54f6
brunoorosco <brunoorosco@gmail.com>
2020-03-27 11:27:48 -0300

    backend Pronto

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./routes')
 
 const app = express();
 
-
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(router)
